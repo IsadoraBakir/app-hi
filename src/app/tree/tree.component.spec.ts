001b985a82f51d4c44bc76392f443525357d9a3a
Isadora Bakir de Santana <isadorabakir@gmail.com>
2021-09-16 07:49:53 -0300

    Ajustando test unitário do componente tree

--- src/app/tree/tree.component.spec.ts
+++ src/app/tree/tree.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TreeComponent } from './tree.component';
@@ -8,7 +9,10 @@ describe('TreeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TreeComponent ]
+      declarations: [TreeComponent],
+      imports: [
+        HttpClientModule
+      ]
     })
     .compileComponents();
   });
